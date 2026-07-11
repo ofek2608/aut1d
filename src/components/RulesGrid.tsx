cb22d548850eb8e2fdf1d50ea2a490c27d1f7492
OfekN <ofek2608@gmail.com>
2026-07-11 06:38:10 +0300

    cleanups

--- src/components/RulesGrid.tsx
+++ src/components/RulesGrid.tsx
@@ -4,35 +4,36 @@ import { decodeNeighborhood, displayRuleIndices } from '../rules'
 import StateInput from './StateInput'
 import styles from './RulesGrid.module.css'
 
+function RulesGridEntry(props: { ruleIndex: number }) {
+  const neighborhood = () => decodeNeighborhood(props.ruleIndex, store.config.numParents, store.config.numStates)
+  const outputState = () => store.config.rules[props.ruleIndex]
+
+  return (
+    <div class={styles.entry}>
+      <div class={styles.neighborhood}>
+        <Index each={neighborhood()}>
+          {(state) => <StateInput value={state()} />}
+        </Index>
+      </div>
+      <StateInput
+        variant="output"
+        value={outputState()}
+        onEdit={newState => setRule(props.ruleIndex, newState)}
+        title={`Rule ${props.ruleIndex}: click to cycle (currently ${outputState()})`}
+      />
+    </div>
+  )
+}
+
 export default function RulesGrid() {
   const displayIndices = createMemo(() =>
     displayRuleIndices(store.config.numParents, store.config.numStates, store.ruleMode),
   )
 
   return (
-    <div class={styles.grid}>
+    <div class={styles.grid} style={{ '--num-parents': store.config.numParents }}>
       <For each={displayIndices()}>
-        {(ruleIndex) => {
-          const neighborhood = () =>
-            decodeNeighborhood(ruleIndex, store.config.numParents, store.config.numStates)
-          const outputState = () => store.config.rules[ruleIndex]
-
-          return (
-            <div class={styles.entry}>
-              <div class={styles.neighborhood}>
-                <Index each={neighborhood()}>
-                  {(state) => <StateInput value={state()} />}
-                </Index>
-              </div>
-              <StateInput
-                variant="output"
-                value={outputState()}
-                onEdit={newState => setRule(ruleIndex, newState)}
-                title={`Rule ${ruleIndex}: click to cycle (currently ${outputState()})`}
-              />
-            </div>
-          )
-        }}
+        {ruleIndex => <RulesGridEntry ruleIndex={ruleIndex} />}
       </For>
     </div>
   )
